bf458d02db6c0daee49a5233494c414b2e0163d5
Adityakr0 <139454948+Adityakr0@users.noreply.github.com>
2023-07-19 14:08:28 +0530

    Update checks.sol

--- checks.sol
+++ checks.sol
@@ -1,25 +1,53 @@
-/ SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
 
-contract Checks {
-    uint public limit = 10 ether;
+contract errorHandling_vottingSystem {
+    mapping(string => bool) public hasVoted;
+    mapping(string => uint) public voteCount;
 
-    function getBalance(address _address) public view returns (uint256) {
-        return _address.balance;
+    function vote(string memory candidate, uint _age) external {
+        ageVerify(_age);
+
+        require(!hasVoted[candidate], "You have already voted");
+
+        voteCount[candidate] += 1;
+        hasVoted[candidate] = true;
+
+        assert(voteCount[candidate] > 0);
     }
 
-    error singleTransferLimit(string message, uint limit);
-
-    function transfer(address payable _receiver) public payable {
-        assert(limit == 10 ether);
-        
-        require(msg.value >= 1 ether, "Atlest 1 ether should be sent");
-        
-        _receiver.transfer(msg.value);
-
-        if (msg.value > limit) {
-            revert singleTransferLimit("Single Transfer Limit Exceeded!", limit);
+    function ageVerify(uint _age) public pure {
+        if (_age < 18) {
+            revert("Not Eligible to Vote");
         }
     }
-
 }
+
+contract VotingExample {
+    error InsufficientAge();
+
+    error AlreadyVoted();
+
+    function simulateVoting() external {
+        errorHandling_vottingSystem votingSystem = new errorHandling_vottingSystem();
+
+        try votingSystem.vote("Candidate1", 25) {
+            // Successful vote
+        } catch Error(string memory errorMessage) {
+            revert(errorMessage);
+        }
+
+        try votingSystem.vote("Candidate2", 16) {
+            revert InsufficientAge();
+        } catch Error(string memory errorMessage) {
+            revert(errorMessage);
+        }
+
+        try votingSystem.vote("Candidate1", 30) {
+            revert AlreadyVoted();
+        } catch Error(string memory errorMessage) {
+            revert(errorMessage);
+        }
+    }
+}
+
